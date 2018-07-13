c7c645d9093047bd27afd986f7a74ff56b41bd4c
Alexey Kapustin <djkah11@yandex.ru>
2018-07-13 17:08:48 +0000

    new comands

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import requests
 import subprocess
 import os
 import sched, time
+import threading
+import sys
 
-bot = telebot.TeleBot("token")
+token = str(sys.argv[1])
+bot = telebot.TeleBot(token)
 
 def download_file(url):
     r = requests.get(url, stream=True)
@@ -16,29 +19,34 @@ def download_file(url):
 def regular_call(sc,bot):
     download_file("http://priem.bmstu.ru/UserFiles/registered-magister-Moscow.pdf")
     os.system("pdftotext magister.pdf rr.txt")
-    count_new = int(subprocess.Popen("grep  'ИУ7' rr.txt | wc -l ", \
-                           shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8','ignore'))
+    count_new = int(subprocess.Popen("grep  'ИУ7' rr.txt | wc -l ", shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8','ignore'))
+    count_in = int(subprocess.Popen("grep  'ИУ7-И' rr.txt | wc -l ",shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8','ignore')) 
+    count_cp = int(subprocess.Popen("grep 'ИУ7 (ЦП)' rr.txt | wc -l ", shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8','ignore')) 
     with open('counter.txt', "r+") as counter:
         old_count = int(counter.readline())
         if old_count<count_new:
             counter.seek(0)
             counter.write(str(count_new))
-            bot.send_message(-1001144863254, "Новая заявка на ИУ7! Всего заявок: "+str(count_new))
-    s.enter(600, 1, regular_call,(sc,bot))
+            bot.send_message(-1001144863254, "Новая заявка на ИУ7! Всего заявок: "+str(count_new) +" . Из них иностранцев " + str(count_in) + " , целевиков " + str(count_cp))
+    sc.enter(600, 1, regular_call,(sc,bot))
 
 
-@bot.message_handler(commands=['retards'])
+@bot.message_handler(commands=['getstat','retards'])
 def handle_current_retards_list(message):
-    with open('counter.txt', "r+") as counter:
-        retards = int(counter.readline())
-        bot.send_message(71301900, "На данный момент на ИУ7 документы подали " + str(retards) + " человек")
-        bot.send_message(-1001144863254, "На данный момент на ИУ7 документы подали " + str(retards) + " человек")
+    count_new = int(subprocess.Popen("grep  'ИУ7' rr.txt | wc -l ", shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8','ignore'))
+    count_in = int(subprocess.Popen("grep  'ИУ7-И' rr.txt | wc -l ",shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8','ignore'))
+    count_cp = int(subprocess.Popen("grep 'ИУ7 (ЦП)' rr.txt | wc -l ", shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8','ignore'))
+    bot.reply_to(message," Всего заявок: "+str(count_new) +" . Из них иностранцев " + str(count_in) + " , целевиков " + str(count_cp))
 
+def timer(bot):
+    s = sched.scheduler(time.time, time.sleep)
+    s.enter(5, 1, regular_call, (s,bot))
+    s.run()
 
-s = sched.scheduler(time.time, time.sleep)
-s.enter(600, 1, regular_call, (s,bot))
-s.run()
-
+   
+t=threading.Thread(target=timer, args=(bot,)) 
+t.start()
+bot.polling(none_stop=True)
 
 
 
